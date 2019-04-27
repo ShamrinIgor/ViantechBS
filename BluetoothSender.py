3f31b65e5c562db9509d460a8638991ceb5f3a99
NIE <bloodymilk1337@gmail.com>
2019-04-27 10:33:07 +0300

    out

--- BluetoothSender.py
+++ BluetoothSender.py
@@ -1,7 +1,13 @@
-import subprocess
-#kek
-command = ["ls", "-l"]
-p = subprocess.Popen(command, stdout=subprocess.PIPE)
-text = p.communicate()
-for line in text:
-    print(text)
+# import subprocess
+# #kek
+# command = ["ls", "-l"]
+# p = subprocess.Popen(command, stdout=subprocess.PIPE)
+# text = p.communicate()
+# for line in text:
+#     print(text)
+import  subprocess
+output = subprocess.check_output("ls -a", shell = True )
+output = str(output)
+output = output.split('\\n')
+for i in range(len(output)):
+    print (output[i])
